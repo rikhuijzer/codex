0b3f0777a945b7cebb28c5dc3eb32dc261537977
Rik <rik@huijzer.xyz>
2020-05-28 14:25:33 +0200

    Clean up

--- default.nix
+++ default.nix
@@ -1,53 +1,21 @@
 { pkgs ? import <nixpkgs> {} }:
 
 let
-  # 20.03
   rev = "5272327b81ed355bbed5659b8d303cf2979b6953";
   channel = fetchTarball "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
-  config = {
-    allowBroken = true;
-  };
-  papajaBuildInputs = with pkgs.rPackages; [
-    afex
-    base64enc
-    beeswarm
-    bookdown
-    broom
-    knitr
-    rlang
-    rmarkdown
-    rmdfiltr
-    yaml
-  ];
-  papaja = with pkgs.rPackages; buildRPackage {
-    name = "papaja";
-    src = pkgs.fetchFromGitHub {
-      owner = "crsh";
-      repo = "papaja";
-      rev = "b0a224a5e67e1afff084c46c2854ac6f82b12179";
-      sha256 = "14pxnlgg7pzazpyx0hbv9mlvqdylylpb7p4yhh4w2wlcw6sn3rwj";
-    };
-    # Do not add propagateddBuildInputs = papajaBuildInputs since
-    # it might cause a buffer overflow when calling `devtools::document`.
-    nativeBuildInputs = papajaBuildInputs;
-  };
-  pkgs = import channel { inherit config; };
+  pkgs = import channel {};
   my-r-packages = with pkgs.rPackages; [
-      bookdown
       devtools
       dplyr
       effsize
-      forestplot
       git2r
       gridExtra
-      papaja
       pwr
-      rmdfiltr
       rprojroot
       svglite
       tidyverse
       xts
-      XLConnect # Excel files.
+      XLConnect
       XLConnectJars
   ];
   R-with-my-packages = pkgs.rWrapper.override{
@@ -56,11 +24,9 @@ let
 in pkgs.mkShell {
   name = "env";
   buildInputs = with pkgs; [
-    git
     glibcLocales # Avoids error in `devtools::check()`.
     haskellPackages.pandoc
     haskellPackages.pandoc-citeproc
     R-with-my-packages
-    zip
   ];
 }
--- run_check.sh
+++ run_check.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
 set -e
-# Rscript -e "testthat::test_dir('tests', stop_on_failure=T)"
 Rscript -e "devtools::check()"
